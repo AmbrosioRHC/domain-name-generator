d36778c8b3169cf18b791047067614d7fa35f6db
AmbrosioRHC <ambrosio.rhc@gmail.com>
2024-02-10 19:31:06 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "lil"];
+  let noun = ["jogger", "racoon", "cat"];
+  let dom = [".com", ".cl", ".us"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < dom.length; d++) {
+          console.log(`${pronoun[p]}${adj[a]}${noun[n]}${dom[d]}`);
+        }
+      }
+    }
+  }
 };
